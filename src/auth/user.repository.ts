5bc43f0990f0d3a71bb4397a95a6dcac5d94d730
Christiaan van der Merwe <chrisvdm0410@gmail.com>
2021-04-29 16:54:59 +0200

    end of work day

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,55 @@
+import {
+    ConflictException,
+    InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+
+        const salt = await bcrypt.genSalt();
+
+        const user = this.create({
+            username,
+            salt,
+            password: await this.hashPassword(password, salt),
+        });
+
+        try {
+            await user.save();
+        } catch (error) {
+            // duplicate username
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+    async validateUserPassword(
+        authCredentialsDto: AuthCredentialsDto,
+    ): Promise<string> {
+        const { password, username } = authCredentialsDto;
+
+        const user = await this.findOne({ username });
+
+        if (user && user.validatePassword(password)) {
+            return user.username;
+        } else {
+            return null;
+        }
+    }
+
+    private async hashPassword(
+        password: string,
+        salt: string,
+    ): Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,60 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    Query,
+    UseGuards,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+
+    @Get()
+    getTasks(
+        @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+    ): Promise<Task[]> {
+        return this.tasksService.getTasks(filterDto);
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    ): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
+
+    @Delete('/:id')
+    @HttpCode(204)
+    deleteTask(@Param('id', ParseIntPipe) id: number): void {
+        this.tasksService.deleteTask(id);
+    }
+}
